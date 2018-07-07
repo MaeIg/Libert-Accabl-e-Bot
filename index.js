d966bd21a35a6c6c8919f8a2ac3ab52853dda53a
MaeIg <40902940+MaeIg@users.noreply.github.com>
2018-07-07 03:09:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function randInt(max) {
 }
 
 // Commandes
-var commandes = ['!help', '!forum', '!Golgoth', '!Maelg', '!Mackay', '!Roventa', '!Sibaal'];
+var commandes = ['!help', '!forum', '!Golgoth', '!Maelg', '!Mackay', '!Roventa', '!Sibaal', '!Cleme', '!Khran'];
 var asw = {
 	help: ['Commandes disponibles :\n' + commandes.join('\n')],
 	forum: ['www.kirov-meta.forumactif.org'],
@@ -16,21 +16,14 @@ var asw = {
 	Maelg: ['Miou !'],
 	Mackay: ["L'humanité est pourrie l'amour est une illusion", "Je vous suis infiniment supérieur ", "Loué soit le grand MacKay", "Ma vie pour MacKay"],
 	Roventa: ["Je t'aime... mais lâche-moi bordel !", "Cool une vodka, je ne serais pas obligé de boire mon eau pour finir l'expé !", "Les russes ont triché l'Égypte devait gagner"],
-	Sibaal: ['Et en plus tu m\'traîte de Golgoth.']
+	Sibaal: ['Et en plus tu m\'traîte de Golgoth.'],
+	Cleme: ['Et c\'est sur ces mots que toute envie révolutionnaire s\'éteint chez le peuple.','Le mandat de la présidente Cleme était incontestable et sa puissance sans limite.', "Île-de-France? Tu veux dire Paris"],
+	Khran: ["Un vrai ETListe abandonne son stuff dans le désert !"]
 };
 var mat100 = 0;
 
 bot.on('message', function (msg) {
 	var txt = msg.content;
-	
-	if (commandes.indexOf(txt) != -1) {
-		if (txt.substr(1) === msg.author.username) {
-			msg.channel.send('Bah c\'est toi idiot :calim:');
-		} else {
-			var L = asw[txt.substr(1)];
-			msg.channel.send(L[randInt(L.length)]);
-		}
-	}
 	
 	if (txt === ':mat100:') {
 		if (mat100 == 2) {
@@ -39,9 +32,21 @@ bot.on('message', function (msg) {
 		} else {
 			mat100 ++;
 		}
+	} else if (txt === '!forum') {
+		msg.channel.send('www.kirov-meta.forumactif.org', {
+			embed: {
+				title: 'www.kirov-meta.forumactif.org',
+				url: 'www.kirov-meta.forumactif.org'
+			});
+	} else if (commandes.indexOf(txt) != -1) {
+		if (txt.substr(1) === msg.author.username) {
+			msg.channel.send('Bah c\'est toi idiot :calim:');
+		} else {
+			var L = asw[txt.substr(1)];
+			msg.channel.send(L[randInt(L.length)]);
+		}
 	}
 });
 
 // Login
 bot.login('NDY0ODM1NjA5Mzg3NTk3ODI0.DiEw5w.45GzHLql2jAuxBU3ADc4pEHsoIM');
-
