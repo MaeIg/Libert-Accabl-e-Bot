5ede8c296ffa03d6e5374d28521b5b6a0ecab604
MaeIg <40902940+MaeIg@users.noreply.github.com>
2018-07-10 04:57:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Initialisation
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-var auth = require("./config/auth.json");
+var auth = require("config/auth.json");
 
 // Fonctions utiles
 function randInt(max) {
