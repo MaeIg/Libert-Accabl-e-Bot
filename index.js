40d4d5dc29f06e8ff7b6f00b1690f5dc438c9e6f
MacKay <alexandrerame77@yahoo.fr>
2018-07-11 15:47:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var asw = {
 	Dragon: ['Coup critique !', 'Bon je vais me coucher, bonne journée !', 'Oh, j\'ai encore fait un 20 aux dés !'],
 	Matsam: ['Hartychou '+sheart, 'Paint '+sheart, 'Ragots '+sheart, 'Maelg le dieu de l\'anti-picto', 'Jpp de ma vie '+scalim],
 	Shaggyz: ["Quel fléau la canicule parisienne, heureusement que je suis dans ma piscine au soleil", "Je suis mort dehors J1 mais c'est pas ma faute je dormais chez ma copine"],
-	Uff: ['Force et honneur !']
+	Uff: ['Force et honneur !'],
 	Hartyom: ['JE SUIS PRISONNIER DU CANAL AUDIO, O SECOUR !']
 };
 var mat100 = 0;
