b9c1d8912901ad888224246b3aca6daaafca1a2f
MaeIg <40902940+MaeIg@users.noreply.github.com>
2018-07-13 17:21:11 +0200

    Update profil.js

--- profil.js
+++ profil.js
@@ -11,7 +11,7 @@ client.connect((err) => {
 });
 client.query('SELECT id FROM members WHERE name LIKE "Maelg"', (err, res) => {
 	if (err) {
-		console.log(err.stack);
+		console.log('test');
 	} else {
 		console.log(res)
 	}
