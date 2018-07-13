203951fb337b321ffb7ae997c314833e49cc96b5
MaeIg <40902940+MaeIg@users.noreply.github.com>
2018-07-13 23:30:39 +0200

    Update profil.js

--- profil.js
+++ profil.js
@@ -14,6 +14,23 @@ client.connect((err) => {
 		console.log('connected');
 	}
 });
+client.query('UPDATE members SET lvl=1, xp=0, messages=1 WHERE id=\'131771591452393472\'', (err) => {
+	if (err) {
+		console.log(err.stack);
+	}
+});
+client.query('UPDATE members SET lvl=1, xp=0, messages=1 WHERE id=\'464835609387597824\'', (err) => {
+	if (err) {
+		console.log(err.stack);
+	}
+});
+client.query('SELECT * FROM members', (err,res) => {
+	if (err) {
+		console.log(err.stack);
+	} else {
+		console.log(res);
+	}
+});
 
 // Fonctions
 function lvlUp (msg, lvl) {
