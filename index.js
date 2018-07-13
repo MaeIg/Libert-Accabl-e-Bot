a15f09e2c5668c31cb471521f9d542373f9c6ff3
MaeIg <40902940+MaeIg@users.noreply.github.com>
2018-07-13 16:58:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const key = process.env.token;
-const profil = require(profil.js);
+const profil = require('profil.js');
 
 // Fonctions utiles
 function randInt(max) {
