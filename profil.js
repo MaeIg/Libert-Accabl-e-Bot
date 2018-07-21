0562785150004f400c92428dbd36057eeb3161aa
MaeIg <40902940+MaeIg@users.noreply.github.com>
2018-07-21 13:11:08 +0200

    Update profil.js

--- profil.js
+++ profil.js
@@ -16,21 +16,6 @@ client.connect((err) => {
 });
 
 
-client.query('CREATE TABLE commands(name text, nbrUses int, lastUse DATE(), userID text)', (err) => {
-	if (err) {
-		console.log(err.stack);
-	} else {
-		console.log('table created');
-	}
-});
-	     
-client.query('SELECT * from commands', (err,res) => {
-	if (err) {
-		console.log(err.stack);
-	} else {
-		console.log(res);
-	}
-});
 
 // Fonctions
 function lvlUp (msg, lvl) {
