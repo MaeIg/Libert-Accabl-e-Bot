4a023236000521140a8a6581dca5912c0a839a26
MacKay <alexandrerame77@yahoo.fr>
2018-09-15 23:55:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,6 @@ bot.on('message', function (msg) {
 		else if(msg.author.username === 'MacKay')
 		{
 			msg.channel.send('8===========================================D');
-		}
 		} else {
 			let nbr = randInt(10) + 1;
 			let penis = '8';
