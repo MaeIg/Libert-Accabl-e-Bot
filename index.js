e7231d69217e9da6a10d1a54a7ef297906bebbbf
MaeIg <40902940+MaeIg@users.noreply.github.com>
2020-11-28 18:33:15 +0100

    Création de !film, !Mad, !Freez, !Mzboub
    Test aussi de checkAnniversaire comme la bdd en dev ne marche plus..

--- index.js
+++ index.js
@@ -30,10 +30,14 @@ const smhh = '🍆';
 const sumbrella = '☔';
 const sdrop = '💦';
 const sscream = '😱';
+const shumpf = "<:humpf:568317177741049856>";
+
+// Salons (chans)
+const ccinema = "<#514690268201746432>";
 
 // Commandes
-const commandes = ['!help', '!forum', '!trombi', '!site', '!requete', '!d + nombre', '!everyone', '!echo', '!cui', '!nuclear', '!cyanure', '!bleus', '!actualité', '!log', '!classement', '!commandes', '!rich', '!profil _nomDiscord', '!penis', '!oreoplz', '!parapluie', '!license'],
-    cpseudo = ['!Bernard', '!Golgoth', '!Maelg', '!MacKay', '!Roventa', '!Sibaal', '!Cleme', '!Khran', '!Drac', '!Dragon', '!Matsam', '!Shaggyz', '!Uff', '!Hartyom', '!Tephal', '!Cerfpihier', '!Lilith', '!Loko', "!WassaW", "!Nathan", "!Zomzom", "!Pellaeon", "!Roystar", "!Dikryl", "!Cornet", "!Ganapati"],
+const commandes = ['!help', '!forum', '!trombi', '!site', '!requete', '!d + nombre', '!everyone', '!echo', '!cui', '!nuclear', '!cyanure', '!bleus', '!actualité', '!log', '!classement', '!commandes', '!rich', '!profil _nomDiscord', '!penis', '!oreoplz', '!parapluie', '!film', '!license'],
+    cpseudo = ['!Bernard', '!Golgoth', '!Maelg', '!MacKay', '!Roventa', '!Sibaal', '!Cleme', '!Khran', '!Drac', '!Dragon', '!Matsam', '!Shaggyz', '!Uff', '!Hartyom', '!Tephal', '!Cerfpihier', '!Lilith', '!Loko', "!WassaW", "!Nathan", "!Zomzom", "!Pellaeon", "!Roystar", "!Dikryl", "!Alexis", "!Ganapati", "!Mad", "!Mzboub", "!Freez"],
     cinvis = ['!membres', '!logs'];
 // commandes => Pour les commandes basiques ; cpseudo => Pour les commandes liées au pseudo ; cinvis => Pour les commandes qui n'apparaissent pas dans le !help
 const asw = {
@@ -48,39 +52,43 @@ const asw = {
 	nuclear: ['Alex The Autist vous détruira tous !'],
 	cyanure: ["J'attends toujours que Matsam me crée " + ssob],
 	bleus: ['On est les champions ! On est les champions ! On est... on est... on est les champions ! ♫', 'Allez les bleus !', '1998-2018 !', 'Et 1... Et 2... Et 3... et 4-2 !'],
-	actualité: ['Google est ton ami !', 'Golgoth est toujours privé d\'ordinateur', 'Hartyom n\'a toujours pas fini de se plaindre', 'Une TRAHISON a eu lieu dans la méta', 'Une deuxième TRAHISON a eu lieu dans la méta', "Il paraîtrait qu'un membre est impliqué dans les deux TRAHISONS récentes...\n||Je ne donnerai pas de nom mais écrivez !Zomzom pour plus de renseignements||"],
+	actualité: ['Google est ton ami !', 'Hartyom n\'a toujours pas fini de se plaindre', "On est en guerre."],
 	log: ['***Last update : 13/02/2020 22h35***\n\nAjout des commandes !Roystar, !Dikryl, !Cornet et !Ganapati.\nMise à jour des commandes !Pellaeon, !Zomzom, !WassaW, !Maelg, !Lilith, !Matsam et !actualité.\n\n_!logs_ pour plus de logs'],
 	logs: ['https://bernard-site.herokuapp.com/logs'],
 	penis: ['8=D'],
 	oreoplz: [soreo],
 	parapluie: ['||' + skoukou + '||' + '||' + smhh + '||' + '||' + sdrop + '||' + '||' + sumbrella + '||'],
+	film: ["You shit on my garden ?!", "Ils sont entrain de manger ma maman" + ssadcat, "Don't piss on the hospitality", "Vous êtes un koala tout à fait décent", "La rivière de nos larmes coule après le koala", "Beaux yeux belles couilles"],
 	license: ['https://github.com/MaeIg/Libert-Accabl-e-Bot/blob/master/LICENSE.md'],
 	Bernard: ['C\'est moi !'],
 	Golgoth: ['Tu parle à ki ?', 'tu me parle à moi?', "j'suis un gangster"],
-	Maelg: ['Miou !', "J'aime pas Despacito, je l'ai juste jouée 50 fois sur osu!", ":3", "Tu veux voir mon babobab ?", "Ma copine c'est encore pire elle peut les écarter à fond", "En vrai jsuis sûr que j'aimerais bien la soupe de bienvenue"],
+	Maelg: ['Miou !', "J'aime pas Despacito, je l'ai juste jouée 50 fois sur osu!", ":3", "Tu veux voir mon babobab ?", "Ma copine c'est encore pire elle peut les écarter à fond", "En vrai jsuis sûr que j'aimerais bien la soupe de bienvenue", "Pourquoi tout ce qui tourne autour de Supken est d'une débilité incommensurable ?", "Wesh c'est mon île pas un logement social"],
 	MacKay: ["L'humanité est pourrie l'amour est une illusion", "Je vous suis infiniment supérieur ", "Loué soit le grand MacKay", "Ma vie pour MacKay", "Et si on abandonnait Matsang pour la quinzième fois ?", "Les éclaireurs : poules mouillées ou tafioles?"],
 	Roventa: ["Je t'aime... mais lâche-moi bordel !", "Cool une vodka, je ne serais pas obligé de boire mon eau pour finir l'expé !", "Les russes ont triché l'Égypte devait gagner", "t'habite ou ?", "T'as du style, épouse-moi !", "Cool un RP ! Jump antipictoquoi ?","Ça m énerve pcq je ne suis pas à la hauteur de tout ce que tu nous apporte", "Une fille qui joue a hordes, ca ne peut pas etre qq'un de manipulateur et d' égocentrique", "Allez allez vous faites moins les malines quand vous nous mangez le sexe"],
 	Sibaal: ['Et en plus tu m\'traîte de Golgoth.'],
-	Cleme: ['Et c\'est sur ces mots que toute envie révolutionnaire s\'éteint chez le peuple.','Le mandat de la présidente Cleme était incontestable et sa puissance sans limite.', "Île-de-France? Tu veux dire Paris", "Y a que Paint de vrai", chanBeauf, "Je préfère le gratin dauphinois à la bite "+sokhand, "CLIQUE SUR LES CHARIOTS SALE CON"],
+	Cleme: ['Et c\'est sur ces mots que toute envie révolutionnaire s\'éteint chez le peuple.','Le mandat de la présidente Cleme était incontestable et sa puissance sans limite.', "Île-de-France? Tu veux dire Paris", "Y a que Paint de vrai", chanBeauf, "Je préfère le gratin dauphinois à la bite "+sokhand, "CLIQUE SUR LES CHARIOTS SALE CON", "Tant mieux qu'elle soit morte ils devraient tous mourir ces sales americains du Sud"],
 	Khran: ['Un vrai ETListe abandonne son stuff dans le désert !', "Meurt dans le désert"],
 	Drac: ['+37 !', "J'ai disparu en 1937 !"],
 	Dragon: ['Coup critique !', 'Bon je vais me coucher, bonne journée !', 'Oh, j\'ai encore fait un 20 aux dés !'],
 	Matsam: ['Hartychou '+sheart, 'Paint '+sheart, 'Ragots '+sheart,'Smash Bros '+sheart, 'Maelg le dieu de l\'anti-picto', 'Jpp de ma vie '+scalim, "Je vais en faire un meme"+smat100, "Pourquoi on est J2 ? On jump pas ce soir ?", soppression, "Perso j'ai deux manières de plier, une manière \"propre\" et une manière \"mi-propre\". Comme ça, quand je prends le vêtement, en fonction de la manière dont il est plié, je sais comment il est", "LES GENS QUI CRIE TOUS DES CON", "ALERTE CANDICE A APPORTÉ...", "JE SUIS SUR MON ÎLE T'ES DANS TA BILE", "J'entends ça je tombe par terre sans vouloir être dramatique", "Vous avez déjà éternué en rigolant ?", "Avant j'étais un gros fayot et maintenant je suis juste une crotte inactive, j'aime beaucoup"],
 	Shaggyz: ["Quel fléau la canicule parisienne, heureusement que je suis dans ma piscine au soleil", "Je suis mort dehors J1 mais c'est pas ma faute je dormais chez ma copine", "Je suis mort dehors mais c'est pas ma faute c'était la finale"],
-	Uff: ['Force et honneur !'],
-	Hartyom: ['JE SUIS PRISONNIER DU CANAL AUDIO, O SECOUR !'+scalim +scalim +scalim, 'VOUS CONNAISSEZ LE POINT COMMUN ENTRE UFF ET LE FROMAGE DE MACKAY ? ON EN ENTEND BEAUCOUP PARLER MAIS ON LES VOIT JAMAIS HAHAHAHAHAHAHAHHAAH', 'Y\'a moyen de pécho dans la méta alors ? Ptain c\'est de mieux en mieux', 'Il ne peut en rester AUCUN. Je ne laisserai aucune de ces racailles faire la loi dans cette ville. NOT UNDER MY WATCH', "Ger une revandication.", "JE VOUS BEZ", "GREVEGENERALE"],
+	Uff: ['Force et honneur !', "Génial les smoothies au foutre !"],
+	Hartyom: ['JE SUIS PRISONNIER DU CANAL AUDIO, O SECOUR !'+scalim +scalim +scalim, 'VOUS CONNAISSEZ LE POINT COMMUN ENTRE UFF ET LE FROMAGE DE MACKAY ? ON EN ENTEND BEAUCOUP PARLER MAIS ON LES VOIT JAMAIS HAHAHAHAHAHAHAHHAAH', 'Y\'a moyen de pécho dans la méta alors ? Ptain c\'est de mieux en mieux', 'Il ne peut en rester AUCUN. Je ne laisserai aucune de ces racailles faire la loi dans cette ville. NOT UNDER MY WATCH', "Ger une revandication.", "JE VOUS BEZ", "GREVEGENERALE", "Quitte à être zoophile tu pourrais te renseigner"],
 	Tephal: ['tg','Les meilleurs articles pour pécho s\'achètent chez Moulinooooooooot.', 'VTFF', 'Ce sera répété, amplifié et déformé ou inversement réciproquement dans l\'autre sens, au choix'],
 	Cerfpihier: ['Écoute petit...'],
 	Lilith: ["Je suis aussi douée avec ma langue, on peut bien s'entendre" + sfeu, "Je suis très flexible sur les horaires (pas que sur les horaires " + ssmile + " )", "J'étais bourrée xD", 'Là où je passe, les PMV trépassent', "Hordes va disparaître en 2020 et j'aurai pas de titre sm " + ssadcat, "Elle est bonne la coquine ?", "T'es plus mon crush", "Jcrois qu'ils ont parlé de moi en rujono hier\nEn ruinoln\nEn Réunion."],
 	Loko: ['On est des gentils ou des mechants ?', 'Bravo Hartyom t\'es le plus fort de la ville', 'Ça freine mes débuts de serial killer','allez violé moi', "Je viens de recompter tu m'as deja sauvé 3 fois donc la prochaine c'est bon tu peux me laisser mourir", 'Manque plus qu\'on me viole dans le désert et je suis au top', 'Shaggyz Cleme je vous love ' + sheart + ' ' + sheart + ' coeur sur vous'],
-	WassaW: ['J AI LU PENDANT 72 HEURES NON STOP AVEC UN SOIR DANS LA NUIT BLANCHE', 'Fais un pavé s\'il te plait', 'NEGATIF IL EST TOUJOURS SOUS PLASTIQUE ON Y TOUCHE PAS', 'AHAHAHAHAH', "Je peux manger du fromage blanc en attendant que tu comprennes ?", "Il vaut mieux aller élever des escargots à Auxerre avec un doctorat plutôt que sans", "Je n'arrive pas à me concentrer de tomate", "Non je vote pas sur Allocine j'aime pas le jaune je deteste ce site", sheart + " Vous êtes mes petits gėnies du DiDi " + sheart, "Ils s'auto dépriment sans moi !", "Pour que deux Moi fassent un Nous il faut que les Moi soi soi"],
+	WassaW: ['J AI LU PENDANT 72 HEURES NON STOP AVEC UN SOIR DANS LA NUIT BLANCHE', 'Fais un pavé s\'il te plait', 'NEGATIF IL EST TOUJOURS SOUS PLASTIQUE ON Y TOUCHE PAS', 'AHAHAHAHAH', "Je peux manger du fromage blanc en attendant que tu comprennes ?", "Il vaut mieux aller élever des escargots à Auxerre avec un doctorat plutôt que sans", "Je n'arrive pas à me concentrer de tomate", "Non je vote pas sur Allocine j'aime pas le jaune je deteste ce site", sheart + " Vous êtes mes petits gėnies du DiDi " + sheart, "Ils s'auto dépriment sans moi !", "Pour que deux Moi fassent un Nous il faut que les Moi soi soi", 'Toujours quand je suis sérieux on me "dé-sérieux"', "On veut juste mon corps parce que je suis bonne, grâce à vous j'ai compris HAHAHAHAHAH", "j ai fait des photos mais c'est pas très joli"],
 	Nathan: ["@everyøne J'ai besoin de garants pour ma sortie de prison ! AIDEZ MOI"],
-	Zomzom: ["Je tiens à dire que \"" + soppression + "\" c'est opressant mais que le \"wesh\" a la fin de ta phrase m'a mis au fond", "C'est à force de lire les magouilles de Shaggyz, j'apprends", "Bah non a une soirée j'avais trop bu je l'ai embrassé, la boulette", "Bonjour la méta, je suis à un hâle et malgré l'heure bon taudive, j'ai bu bien :D Désolé de ma mort de noob lors du précédent CM :'( ça m' artiste !", "Vous me faites trop rire je vous aime aussi haha.", "Je viens pas bourré ici okay", "Le masque c'est comme l'apres shampooing : c'est sous la douche", "L'absence d'info est une info tant que c'est communiqué."],
-	Pellaeon: ["\"caquelon\", c'est un poulet travesti qui caquette ?", "Sous word il y a deux \"espaces\", un avec \"espace\", un second généré automatique, comme quand tu met des \"guillements\" et qui se place tout seul", "Je tente de mettre en demeure sfr de revoir toutes les resiliations de l'année devant la repression des fraudes ? X)", "George Clone né ?", "Arf, je décrète officiellement la creation du pays des imaginaire alors clochette !", "Comme une expé : avec un fouet, de la volonté et des MSE !", "La logique et le bon sens n'ont pas de place dans cette maison.", "Plus de licornes et moins d'argent au quotidien ?", "Vous êtes Rabin en informations mademoiselle. Mais ça correspond au personne, j'apprécie la logique !", "C'est du pas toi local ?", "Même s'il est ironique ee moins de vue de ne préparer l'armée qu'a un changement annuel, de moins point de vue xD"],
-	Roystar: ["Ptain j'ai mis mon caleçon à l'envers ce matin jme disais bien que c'était bizarre", "Ah mais la prochaine fois je ramène du champomy moi vous m'avez fait trop peur jme souviens pas de tout " + sscream],
+	Zomzom: ["Je tiens à dire que \"" + soppression + "\" c'est opressant mais que le \"wesh\" a la fin de ta phrase m'a mis au fond", "C'est à force de lire les magouilles de Shaggyz, j'apprends", "Bah non a une soirée j'avais trop bu je l'ai embrassé, la boulette", "Bonjour la méta, je suis à un hâle et malgré l'heure bon taudive, j'ai bu bien :D Désolé de ma mort de noob lors du précédent CM :'( ça m' artiste !", "Vous me faites trop rire je vous aime aussi haha.", "Je viens pas bourré ici okay", "Le masque c'est comme l'apres shampooing : c'est sous la douche", "L'absence d'info est une info tant que c'est communiqué.", "Ce qui est plus chelou c'est ton aviq sur les viuex et le corona", "INHUSTICR"],
+	Pellaeon: ["\"caquelon\", c'est un poulet travesti qui caquette ?", "Sous word il y a deux \"espaces\", un avec \"espace\", un second généré automatique, comme quand tu met des \"guillements\" et qui se place tout seul", "Je tente de mettre en demeure sfr de revoir toutes les resiliations de l'année devant la repression des fraudes ? X)", "George Clone né ?", "Arf, je décrète officiellement la creation du pays des imaginaire alors clochette !", "Comme une expé : avec un fouet, de la volonté et des MSE !", "La logique et le bon sens n'ont pas de place dans cette maison.", "Plus de licornes et moins d'argent au quotidien ?", "Vous êtes Rabin en informations mademoiselle. Mais ça correspond au personne, j'apprécie la logique !", "C'est du pas toi local ?", "Même s'il est ironique ee moins de vue de ne préparer l'armée qu'a un changement annuel, de moins point de vue xD", "Ca dépend, si son signe astrologique est le cancer, c'est p'têt une version française Symfony", "Que nenniema mie"],
+	Roystar: ["Ptain j'ai mis mon caleçon à l'envers ce matin jme disais bien que c'était bizarre", "Ah mais la prochaine fois je ramène du champomy moi vous m'avez fait trop peur jme souviens pas de tout " + sscream, "t ban"],
 	Dikryl: ["Salutérus, ça vagin ?"],
-	Cornet: ["On a déjà vomi dans mon duvet et dans mes oreilles c'était vraiment pas super " + ssadcat, "Voulez vous écouter \"on va niquer ce soir\" ?"],
-	Ganapati: ["Merci Harty, n'oublions pas la \"recrue décence\", à l'heure où la recrudescence du molestage et du bizutage bat son plein ! xD"]
+	Alexis: ["On a déjà vomi dans mon duvet et dans mes oreilles c'était vraiment pas super " + ssadcat, "Voulez vous écouter \"on va niquer ce soir\" ?", "TU CROIS QUE TU COMPRENDS PAS MAIS EN FAIT TU COMPRENDS PAS ENCORE MOINS"],
+	Ganapati: ["Merci Harty, n'oublions pas la \"recrue décence\", à l'heure où la recrudescence du molestage et du bizutage bat son plein ! xD"],
+	Mad: ["Personne ira visiter ton île.", shumpf],
+	Mzboub: ["!menbre"],
+	Freez: ["la sélection naturelle c'est le fait qu'un organisme survivre dans un milieu et pas un autre organisme, selon moi"]
 };
 const helpInfo = {
 	help: "**Utilisation**\n```!help _Commande```\n**Description**\n```La commande help donne des informations sur l'utilisation de la commande spécifiée. Si aucune commande n'est spécifiée, elle donne la liste des commandes disponibles.```",
@@ -107,9 +115,14 @@ const helpInfo = {
 	commandes: "**Utilisation**\n```!commandes```\n**Description**\n```Donne la liste des commandes les plus utilisées sur ce discord.```",
 	classement: "**Utilisation**\n```!classement```\n**Description**\n```Donne la liste des personnes ayant le plus parlé sur ce discord. Qui arrivera à détrôner Cleme ?```",
 	requete: "**Utilisation**\n```!requete Nom Phrase```\n**Description**\n```Si vous voulez qu'une phrase dite par un joueur lui soit associée sur Bernard, vous pouvez utiliser cette commande. Si la communauté approuve, ces phrases seront ajoutées à Bernard. Comme ça les personnes qui ne sont pas dans les phrases de Bernard n'auront plus de raison de se plaindre !!```\n**Exemple**\n```!requete Khayrisill Tu me manques wawa```",
-	site: "**Utilisation**\n```!site```\n**Description**\n```Envoit un lien vers l'antre de Bernard. Qui sait ce que vous pourriez y trouver ?```"
+	site: "**Utilisation**\n```!site```\n**Description**\n```Envoie un lien vers l'antre de Bernard. Qui sait ce que vous pourriez y trouver ?```",
+	film: "**Utilisation**\n```!film```\n**Description**\n```Envoie une citation aléatoire de l'un des films vu dans " + ccinema + "```"
 };
 var mat100 = 0;
+
+// CRON
+setInterval(profil.checkAnniversaire, 1500);
+
 
 bot.on('message', function (msg) {
 	profil.newMessage(msg);
