af04610f8548c11209d7462a687bc6c5cb04aaed
MaeIg <larochmael@hotmail.fr>
2021-12-26 20:55:47 +0100

    refactor(help): remove a useless condition

--- src/commands/specialCommands/help.js
+++ src/commands/specialCommands/help.js
@@ -6,20 +6,15 @@ const help = (message, command) => {
     const resultList = helpInfo[command.slice(6)];
     if (resultList !== undefined) {
       message.channel.send(resultList);
-    } else {
-      message.channel.send(
-        `**Commandes disponibles :**\n\`\`\`${commandList.join(
-          "\n"
-        )}\`\`\`\n_!membres_ pour plus de commandes\n_!help Commande_ pour plus d'information sur une commande`
-      );
+      return;
     }
-  } else {
-    message.channel.send(
-      `**Commandes disponibles :**\n\`\`\`${commandList.join(
-        "\n"
-      )}\`\`\`\n_!membres_ pour plus de commandes\n_!help Commande_ pour plus d'information sur une commande`
-    );
   }
+
+  message.channel.send(
+    `**Commandes disponibles :**\n\`\`\`${commandList.join(
+      "\n"
+    )}\`\`\`\n_!membres_ pour plus de commandes\n_!help Commande_ pour plus d'information sur une commande`
+  );
 };
 
 export { help };
